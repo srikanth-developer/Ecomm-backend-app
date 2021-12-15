ad95db69d3869acb630935e749d386fb09722cd5
srikanth-developer <srikanthreddy233k@gmail.com>
2021-12-15 10:55:52 +0530

    update changes

--- index_server.js
+++ index_server.js
@@ -5,11 +5,15 @@ const app = express();
 //environment variables from .env file
 const env = require("dotenv");
 env.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000
 
 //body-parser to parse the request body
 // app.use(express.json())
 app.use(express.json())
+
+// db connection
+require('./config/db.js')
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "welcome to the landing page",
