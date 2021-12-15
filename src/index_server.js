bac980a7739cb5ec77d8d35a57ba0547b86f7f9a
srikanth-developer <srikanthreddy233k@gmail.com>
2021-12-15 10:25:35 +0530

    bodt-parser

--- src/index_server.js
+++ src/index_server.js
@@ -1,3 +1,4 @@
+//to create a server
 const express = require("express");
 const app = express();
 
@@ -6,9 +7,17 @@ const env = require("dotenv");
 env.config();
 const port = process.env.PORT;
 
+//body-parser to parse the request body
+// app.use(express.json())
+app.use(express.json())
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "welcome to the landing page",
+  });
+});
+app.post("/data", (req, res) => {
+  res.status(200).json({
+    message: req.body,
   });
 });
 app.listen(port, () => {
